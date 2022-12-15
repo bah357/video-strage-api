b41245a27a3a9eb8c0709109acf6fdc7547b1595
kanjana.nookaew <bah357@gmail.com>
2022-12-15 15:52:30 +0700

    fix github action (4)

--- src/main/java/com/demo/api/configuration/SecurityConfiguration.java
+++ src/main/java/com/demo/api/configuration/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeHttpRequests(httpRequests -> httpRequests
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
